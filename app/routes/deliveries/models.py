1562cd631d1751ce3c946006d8bd99abb55136e5
luisffee <luizffeelix@gmail.com>
2024-12-14 20:45:59 -0300

    :sparkles: feat: Backend

--- app/routes/deliveries/models.py
+++ app/routes/deliveries/models.py
@@ -3,3 +3,8 @@ from ...db import db
 class Delivery(db.Model):
     __tablename__ = 'delivery'
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    delivery_address = db.Column(db.String(255), nullable=False)
+    delivery_arrival_date = db.Column(db.Date, nullable=True)
+    delivery_status = db.Column(db.String(16), nullable=True)
+    delivery_type = db.Column(db.String(16), nullable=False)
