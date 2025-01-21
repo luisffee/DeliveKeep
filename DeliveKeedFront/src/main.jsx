187099eb0aac53160b2482111027ae4c86f698b2
WendellGSoares <wendell.soares@gmail.com>
2025-01-21 18:26:16 -0300

    feat pagina atendimentos

--- DeliveKeedFront/src/main.jsx
+++ DeliveKeedFront/src/main.jsx
@@ -8,6 +8,7 @@ import Register from './pages/Register.jsx'
 import Info from './pages/Info.jsx'
 import Endereco from './pages/Endereco.jsx'
 import Pagamento from './pages/Pagamento.jsx'
+import Atendimento from './pages/Atendimento.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/info" element={<Info />} />
         <Route path="/endereco" element={<Endereco />} />
         <Route path="/pagamento" element={<Pagamento />} />
+        <Route path="/atendimento" element={<Atendimento />} />
       </Routes>
     </Router>
   </StrictMode>,
